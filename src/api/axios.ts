f88c34046628294f70c0108dec5d6fdc96aa1729
Pau Serrano <pauserrano14@gmail.com>
2026-02-05 15:39:28 +0100

    added logs for debugging

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,6 +4,8 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5005",
 });
 
+console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
+
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("authToken");
